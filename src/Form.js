e0975051bcb489cc7cfc83f66543ffc4131dd342
Bj'z <zbjarchi@gmail.com>
2021-08-18 10:57:05 +0100

    feat comment

---
+++ src/Form.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  render() {
+    return (
+      <div>
+        Image here
+        <h1>Comment</h1>
+        Form
+      </div>
+    );
+  }
+}
+
+export default Form;
