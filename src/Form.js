f31e099477359e499668e57728d7d3405c26f52b
Bj'z <zbjarchi@gmail.com>
2021-08-18 14:18:25 +0100

    feat select color en dur

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,39 @@
 import React, { Component } from "react";
 import Car from "./Car";
 class Form extends Component {
+  state = {
+    username: "",
+    color: "",
+    colors: ["", "green", "red", "purple", "black", "pink"],
+  };
+
+  handlePseudo = (e) => {
+    this.setState({ username: e.target.value });
+  };
   render() {
     return (
       <div>
         <Car color="blue" height="400" />
         <h1>Comment</h1>
-        Form
+        <form action="">
+          <div>
+            <label htmlFor="">pseudo</label>
+
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.handlePseudo}
+            />
+          </div>
+          <div>
+            <label htmlFor="">color</label>
+            <select name="" id="">
+              <option value="green">green</option>
+              <option value="red">red</option>
+              <option value="purple">purple</option>
+            </select>
+          </div>
+        </form>
       </div>
     );
   }
